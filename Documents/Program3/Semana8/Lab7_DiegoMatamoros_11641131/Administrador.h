618795316ceba909b71fe3144f446b88a851dfc9
DiegoMatamoros <dam_900@unitec.edu>
2017-12-01 21:24:28 -0600

    Empezando2

--- Documents/Program3/Semana8/Lab7_DiegoMatamoros_11641131/Administrador.h
+++ Documents/Program3/Semana8/Lab7_DiegoMatamoros_11641131/Administrador.h
@@ -1,3 +1,28 @@
 #ifndef ADMINISTRADOR_H
 #define ADMINISTRADOR_H
+
+#include <string>
+#include <vector>
 #include "Personal.h"
+
+using namespace std;
+
+class Administrador:public Personal{
+private:
+	vector<Personal>contratados;
+	vector<Personal>despedidos;
+
+public:
+	Administrador(string, string);
+	Administrador();
+
+	vector<Personal> getContratados();
+	vector<Personal> getDespedidos();
+
+	void setContratados(vector<Personal>);
+	void setDespedidos(vector<Personal>);
+
+	virtual ~Administrador();
+
+};
+#endif
